f7e3d2c3986815090546751d3a12c9adb7928193
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-03-27 20:45:26 -0300

    Interpolando variaveis

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function App() {
+  // 1 - variaveis
+  const name: string = "Miguel";
+  const age: number = 62;
+  const isWorking: boolean = true;
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando!</p>
+        </div>
+      )}
     </div>
   );
 }
